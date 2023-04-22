32118f10c082ff469567f3295a664d50b61a0e79
Mateo <mateo.mangado@gmail.com>
2023-04-22 14:58:04 +0200

    22/04

--- javaS/h2.js
+++ javaS/h2.js
@@ -4,17 +4,39 @@ const urlParams = new URLSearchParams(queryString);
 const personnage = urlParams.get('personnage');
 
 // Affichage de l'image correspondante
-const image = document.getElementById('image-personnage');
+const imageS = document.getElementById('image-personnage');
 switch (personnage) {
   case 'p1':
-    image.src = '../img/p1.png';
-    image.alt = 'perso1';
+    imageS.src = '../img/p1.png';
+    imageS.alt = 'perso1';
     break;
   case 'p2':
-    image.src = '../img/p2.jpg';
-    image.alt = 'perso2';
+    imageS.src = '../img/p2.png';
+    imageS.alt = 'perso2';
     break;
-  default:
-    console.error('Personnage inconnu');
 }
 
+var btnVrai = document.getElementById('btnV');
+var btnFaux = document.getElementById('btnF');
+
+var overlay = document.getElementById('overlay');
+var btnClose = document.getElementById('btnClose');
+var popupText = document.getElementById('popupText');
+
+btnVrai.addEventListener('click', function() {
+  openModal("Et oui c'est vrai !");
+});
+
+btnFaux.addEventListener('click', function() {
+  openModal("Hélas non c'est bien vrai");
+});
+btnClose.addEventListener('click', closePopup);
+
+function openModal(text){
+  popupText.innerHTML = text;
+  overlay.style.display = 'block';
+}
+
+function closePopup(){
+  overlay.style.display = 'none';
+}
